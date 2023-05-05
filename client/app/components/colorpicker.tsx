64ececc81591d5797347518d258a563db586c408
Muhammad Aman Yasin <M.AMANYASIN@GMAIL.COM>
2023-05-05 20:15:21 +0200

    saving

--- client/app/components/colorpicker.tsx
+++ client/app/components/colorpicker.tsx
@@ -25,7 +25,7 @@ function ColorPicker({ onChange, color: defaultColor }: ColorPickerProps) {
     }
 
   return (
-    <div>
+    <div className='relative'>
         <div className='flex'>
             <div onClick={handleClick} style={{ backgroundColor: color }} className='border border-gray-400 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer mr-2'></div>
             <div className="w-1 border rounded-md bg-gray-400 mr-2 my-2"></div>
@@ -34,23 +34,9 @@ function ColorPicker({ onChange, color: defaultColor }: ColorPickerProps) {
             <div onClick={() => handleChange({ hex: '#3b82f6'})} className='bg-blue-500 border border-gray-400 rounded-full w-10 h-10 flex justify-center items-center hover:border-gray-800 cursor-pointer mr-2'></div>
         </div>
         {displayColorPicker && (
-            <div
-            style={{
-                position: 'absolute',
-                zIndex: '2',
-            }}
-            >
-            <div
-                style={{
-                position: 'fixed',
-                top: '0px',
-                right: '0px',
-                bottom: '0px',
-                left: '0px',
-                }}
-                onClick={handleClose}
-            />
-            <ChromePicker color={color} onChange={handleChange} />
+            <div className='absolute z-10'>
+                <div className='fixed inset-0' onClick={handleClose} />
+                <ChromePicker className='mt-1' color={color} onChange={handleChange} />
             </div>
         )}
     </div>
